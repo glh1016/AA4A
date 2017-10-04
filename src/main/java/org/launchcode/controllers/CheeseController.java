fa0424b69dccd1ceab777b7c7efc8097e855e71a
Garrett Haas <garrett.l.haas@gmail.com>
2017-10-03 21:41:35 -0500

    100% functionality

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -33,10 +33,8 @@ public class CheeseController {
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
-
         model.addAttribute("cheeses", cheeseDao.findAll());
         model.addAttribute("title", "My Cheeses");
-
         return "cheese/index";
     }
 
