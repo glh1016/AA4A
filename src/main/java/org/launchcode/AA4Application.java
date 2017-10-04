2c9a1eae393641d4152cceae13df603d7b2614f7
Garrett Haas <garrett.l.haas@gmail.com>
2017-10-03 22:22:18 -0500

    first commit

--- src/main/java/org/launchcode/AA4Application.java
+++ src/main/java/org/launchcode/AA4Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheeseMvcApplication {
+public class AA4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CheeseMvcApplication.class, args);
+		SpringApplication.run(AA4Application.class, args);
 	}
 }
--- src/test/java/org/launchcode/AA4ApplicationTests.java
+++ src/test/java/org/launchcode/AA4ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CheeseMvcApplicationTests {
+public class AA4ApplicationTests {
 
 	@Test
 	public void contextLoads() {
