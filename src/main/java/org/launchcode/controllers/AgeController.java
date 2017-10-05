5d617d02dceecdd9caffe167b322ca1ec141c234
Garrett Haas <garrett.l.haas@gmail.com>
2017-10-05 01:21:52 -0500

    commit before adding new mamp database

---
+++ src/main/java/org/launchcode/controllers/AgeController.java
@@ -0,0 +1,10 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by t420-11 on 10/5/2017.
+ */
+@Controller
+public class AgeController {
+}
